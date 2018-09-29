62a967e68439e156250c45d0f48388438eb26df7
halverson-jason <halv.jason@gmail.com>
2018-09-28 21:52:22 -0700

    Wolves Semi-Finished, no hunting yet

--- src/prove02/Wolf.java
+++ src/prove02/Wolf.java
@@ -7,14 +7,55 @@ public class Wolf extends Creature implements Movable,Aware,Aggressor {
 
     Random _rand;
 
+    private Direction preferredDirection;
+
     public Wolf(){
-        _rand = new Random();
+        preferredDirection = initialDirection();
         _health = 1;
+    }
+
+    public Direction initialDirection(){
+        _rand = new Random();
+        Direction initialDirection = Direction.Left;
+        // Choose a random direction to start
+        switch(_rand.nextInt(4)) {
+            case 0:
+                initialDirection = Direction.Left;
+                break;
+            case 1:
+                initialDirection = Direction.Up;
+                break;
+            case 2:
+                initialDirection = Direction.Right;
+                break;
+            case 3:
+                initialDirection = Direction.Down;
+                break;
+            default:
+                break;
+        }
+        return initialDirection;
     }
     // interface implementations
     public void move() {
 
-        _location.x++;
+        // move according to the preferred direction.
+        switch(preferredDirection) {
+            case Left:
+                _location.x--;
+                break;
+            case Up:
+                _location.y++;
+                break;
+            case Right:
+                _location.x++;
+                break;
+            case Down:
+                _location.y--;
+                break;
+            default:
+                break;
+        }
 
     }
 
@@ -25,7 +66,10 @@ public class Wolf extends Creature implements Movable,Aware,Aggressor {
 
     }
     public void attack(Creature target){
-
+        //Attack only animals
+        if(target instanceof Animal) {
+            target.takeDamage(5);
+        }
     }
 
     public Shape getShape() {
